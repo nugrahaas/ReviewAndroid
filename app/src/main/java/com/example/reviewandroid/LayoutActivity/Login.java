b8a221c011ccf70c559acd28e57e3afd6acea4ad
nugrahaas <tyoku1792@gmail.com>
2019-10-22 09:27:26 +0700

    EmailPassword Login and Register is working

--- app/src/main/java/com/example/reviewandroid/LayoutActivity/Login.java
+++ app/src/main/java/com/example/reviewandroid/LayoutActivity/Login.java
@@ -1,20 +1,83 @@
 package com.example.reviewandroid.LayoutActivity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.reviewandroid.Entitas.User;
 import com.example.reviewandroid.MainActivity;
 import com.example.reviewandroid.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
+
+    DatabaseReference databaseUser;
+    EditText editTextUsername, editTextPass;
+    String email_txt,password_txt;
+    private FirebaseAuth mAuth;
+    Button btn_login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        editTextUsername = (EditText) findViewById(R.id.et_username);
+        btn_login = (Button) findViewById(R.id.btn_login);
+        editTextPass = (EditText) findViewById(R.id.et_password);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email_txt = editTextUsername.getText().toString();
+                password_txt = editTextPass.getText().toString();
+                if (TextUtils.isEmpty(email_txt)) {
+                    Toast.makeText(getApplicationContext(), "Masukan Email !!!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password_txt)) {
+                    Toast.makeText(getApplicationContext(), "Masukan Password !!!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.signInWithEmailAndPassword(email_txt, password_txt)
+                        .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Login sukses, masuk ke Main Activity
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Intent intent = new Intent(Login.this, MainActivity.class);
+                                    startActivity(intent);
+                                    Toast.makeText(Login.this, "Login Berhasil", Toast.LENGTH_LONG).show();
+                                    finish();
+                                } else {
+                                    // Jika Login gagal, memberikan pesan
+                                    Toast.makeText(Login.this, "Proses Login gagal : " +  task.getException(), Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+            }
+        });
+
     }
 
     public void Daftar(View view) {
@@ -22,9 +85,12 @@ public class Login extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void Login(View view) {
-        Intent intent = new Intent(Login.this,MainActivity.class);
-        startActivity(intent);
+
+    public void loginUser(){
+
+
 
     }
+
+
 }
--- app/src/main/java/com/example/reviewandroid/LayoutActivity/Register.java
+++ app/src/main/java/com/example/reviewandroid/LayoutActivity/Register.java
@@ -1,29 +1,95 @@
 package com.example.reviewandroid.LayoutActivity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.reviewandroid.Entitas.User;
 import com.example.reviewandroid.MainActivity;
 import com.example.reviewandroid.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Register extends AppCompatActivity {
+
+    DatabaseReference databaseUser;
+    EditText editTextNama, editTextUsername, editTextPass;
+    private FirebaseAuth mAuth;
+    String email_txt,password_txt;
+    Button btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+
+        editTextUsername = (EditText) findViewById(R.id.et_username);
+        editTextPass = (EditText) findViewById(R.id.et_password);
+        btn_register = (Button) findViewById(R.id.btn_register);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email_txt = editTextUsername.getText().toString();
+                password_txt = editTextPass.getText().toString();
+                if (TextUtils.isEmpty(email_txt)) {
+                    Toast.makeText(getApplicationContext(), "Masukan Email !!!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password_txt)) {
+                    Toast.makeText(getApplicationContext(), "Masukan Password !!!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.createUserWithEmailAndPassword(email_txt, password_txt)
+                        .addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Daftar sukses, masuk ke Main Activity
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Intent intent = new Intent(Register.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    // Jika daftar gagal, memberikan pesan
+                                    Toast.makeText(Register.this, "Proses Pendaftaran gagal : " +  task.getException(),
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+            }
+        });
+
     }
 
-    public void Daftar(View view) {
-        Intent intent = new Intent(Register.this, MainActivity.class);
-        startActivity(intent);
-    }
 
     public void Login(View view) {
         Intent intent = new Intent(Register.this, Login.class);
         startActivity(intent);
     }
+    private void registerUser() {
+
+
+
+    }
+
+
+
+
 }
